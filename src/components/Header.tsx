a9fbc777741cbb3ce99dd441374e2456c31ade27
letrungkiemai <l.trung.k.ai@gmail.com>
2026-03-09 20:24:04 +0100

    Mobile tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ai from "../ai.png";
 export const Header = () => {
   const fullHeader = (
     <header className={`sticky top-0 z-50`}>
-      <div className="flex sm:flex-row flex-col container m-auto max-w-none h-52 bg-slate-800 px-20">
+      <div className="flex sm:flex-row flex-col container m-auto max-w-none bg-slate-800 md:px-20 md:py-4">
         <div className="sm:block hidden mt-auto mb-auto ml-8 mr-8 basis-52">
           <a href={"https://linkedin.com/in/ailetrungkiem"} target="_blank" rel="noreferrer" >
             <img className="h-44 rounded-full border-8" src={ai} alt="aile" />
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,7 +5,7 @@ import { Menu } from "./Menu";
 import { Projects } from "./Projects";
 export const Page = () => {
   return (
-    <div className="flex flex-row container m-auto max-w-none h-52 bg-slate-200 h-auto text-slate-900 px-20">
+    <div className="flex flex-row container m-auto max-w-none h-52 bg-slate-200 h-auto text-slate-900 md:px-20">
       <div
         className="sm:block hidden mt-8 mb-auto ml-8 mr-8 basis-60"
         style={{ minWidth: "190px" }}
